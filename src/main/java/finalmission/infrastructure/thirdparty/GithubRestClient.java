03862b8b5235ced3c7a0bee74987796493fbff93
ljhee92 <weilainib7@gmail.com>
2025-09-03 16:50:07 +0900

    fix: github uri 환경별 수정

--- src/main/java/finalmission/infrastructure/thirdparty/GithubRestClient.java
+++ src/main/java/finalmission/infrastructure/thirdparty/GithubRestClient.java
@@ -4,7 +4,6 @@ import finalmission.dto.response.GithubUserResponse;
 import finalmission.dto.response.TokenResponse;
 import finalmission.exception.ApiException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,23 +15,25 @@ public class GithubRestClient {
 
     private static final String GITHUB_LOGIN_URL = "https://github.com/login/oauth";
     private static final String AUTHORIZE_URI = "/authorize";
-    private static final String REDIRECT_URI = "http://localhost:8080/login/github/callback";
     private static final String ACCESS_URI = "/access_token";
     private static final String GITHUB_API_URL = "https://api.github.com";
     private static final String USER_URI = "/user";
 
     private final String clientId;
     private final String clientSecret;
+    private final String redirectUri;
     private final RestClient loginRestClient;
     private final RestClient apiRestClient;
 
     public GithubRestClient(
             @Value("${github.client-id}") String clientId,
             @Value("${github.client-secret}") String clientSecret,
+            @Value("${github.redirect-uri}") String redirectUri,
             RestClient.Builder builder
     ) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
         this.loginRestClient = builder.baseUrl(GITHUB_LOGIN_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
@@ -48,7 +49,7 @@ public class GithubRestClient {
     public String getAuthorizeUrl() {
         return GITHUB_LOGIN_URL + AUTHORIZE_URI
                 + "?client_id=" + clientId
-                + "&redirect_uri=" + REDIRECT_URI
+                + "&redirect_uri=" + redirectUri
                 + "&scope=read:user+user:email";
     }
 
