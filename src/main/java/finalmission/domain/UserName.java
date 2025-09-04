a60fe32299b5414d444f328261d420ec4ba072f8
ljhee92 <weilainib7@gmail.com>
2025-09-04 12:39:40 +0900

    fix: 디폴트 이름 설정

--- src/main/java/finalmission/domain/UserName.java
+++ src/main/java/finalmission/domain/UserName.java
@@ -18,25 +18,37 @@ public class UserName {
     private String name;
 
     private UserName(String name) {
-        validateEmpty(name);
-        validateLength(name);
-        this.name = name;
+        if (validateEmpty(name)) {
+            validateLength(name);
+            this.name = name;
+        }
     }
 
     public static UserName from(String name) {
         return new UserName(name);
     }
 
-    private void validateEmpty(String name) {
+    private boolean validateEmpty(String name) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 사용자의 이름은 빈 값일 수 없습니다.");
+            this.name = generateDefaultName((int) (Math.random() * 3) + 1);
+            return false;
         }
+        return true;
     }
 
     private void validateLength(String name) {
         if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 사용자의 이름은 2자 이상 5자 이하여야 합니다.");
         }
+    }
+
+    private String generateDefaultName(int length) {
+        StringBuilder name = new StringBuilder("사용자");
+        for (int i = 0; i < length; i++) {
+            int digit = (int) (Math.random() * 10);
+            name.append(digit);
+        }
+        return name.toString();
     }
 
     @Override
